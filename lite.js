92eaefb4dd7e706685b4120558fa760f5e5b7790
phong lu <phong_lu@symantec.com>
2017-11-15 14:24:46 -0800

    fix path

--- lite.js
+++ lite.js
@@ -4,14 +4,14 @@ import fetch from 'whatwg-fetch';
 import Promise from 'promise-polyfill';
 import observable from 'riot-observable';
 import route from 'riot-route';
-import Bind from 'static_packages/bind.min.js';
-import jBone from 'static_packages/jbone.min.js';
-import loadResource from 'static_packages/load-resource.js';
-import logger from 'static_packages/console-logger.js';
+import Bind from './static_packages/bind.min.js';
+import jBone from './static_packages/jbone.min.js';
+import loadResource from './static_packages/load-resource.js';
+import logger from './static_packages/console-logger.js';
 
-import { registerApp } from 'src/scripts/espa.js';
-import { setupRoute } from 'src/scripts/espa-route.js';
-import ChildApp from 'src/scripts/espa-child-app.js';
+import { registerApp } from './src/scripts/espa.js';
+import { setupRoute } from './src/scripts/espa-route.js';
+import ChildApp from './src/scripts/espa-child-app.js';
 
 window.ESPA = window.ESPA || {};
 window.ESPA._apps = [];
