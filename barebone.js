c705c86e752df59baa5de7f213ff7585d6414d98
phong_lu <phong_lu@symantec.com>
2017-11-22 18:05:34 -0800

    add observable to barebone package

--- barebone.js
+++ barebone.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import observable from 'riot-observable';
 import route from 'riot-route';
 import Bind from './static_packages/bind.min.js';
 import loadResource from './static_packages/load-resource.js';
@@ -8,7 +9,10 @@ import { getQueryString } from './static_packages/utils.js';
 
 window.ESPA = window.ESPA || {};
 window.ESPA.Bind = Bind;
+window.ESPA.observable = observable;
 window.ESPA.route = route;
 window.ESPA.loadResource = loadResource;
 window.ESPA.getQueryString = getQueryString;
 window.ESPA.logger = logger;
+
+window.ESPA.observable(window.ESPA);
