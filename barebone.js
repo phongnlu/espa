d97e576dca9560f189962f24e56547cd00b33e1d
phong_lu <phong_lu@symantec.com>
2017-11-29 19:11:40 -0800

    update barebone package

--- barebone.js
+++ barebone.js
@@ -1,17 +1,13 @@
 'use strict';
 
 import observable from 'riot-observable';
-import route from 'riot-route';
-import Bind from './static_packages/bind.min.js';
 import loadResource from './static_packages/load-resource.js';
 import logger from './static_packages/console-logger.js';
 import tmpl from './static_packages/micro-template.js';
 import { getQueryString } from './static_packages/utils.js';
 
 window.ESPA = window.ESPA || {};
-window.ESPA.Bind = Bind;
 window.ESPA.observable = observable;
-window.ESPA.route = route;
 window.ESPA.loadResource = loadResource;
 window.ESPA.getQueryString = getQueryString;
 window.ESPA.logger = logger;
