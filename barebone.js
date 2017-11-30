27679779780ddccc796463dd57718e5c2f8c8c9c
phong_lu <phong_lu@symantec.com>
2017-11-30 13:30:24 -0800

    add riot route+observable to barebone package

--- barebone.js
+++ barebone.js
@@ -1,15 +1,17 @@
 'use strict';
 
-import observable from 'riot-observable';
+import './static_packages/riot-route-observable.js';
 import loadResource from './static_packages/load-resource.js';
 import logger from './static_packages/console-logger.js';
 import tmpl from './static_packages/micro-template.js';
-import { getQueryString } from './static_packages/utils.js';
+import { getQueryString, navigate, registerRoute  } from './static_packages/utils.js';
 
 window.ESPA = window.ESPA || {};
 window.ESPA.observable = observable;
 window.ESPA.loadResource = loadResource;
 window.ESPA.getQueryString = getQueryString;
+window.ESPA.navigate = navigate;
+window.ESPA.registerRoute = registerRoute;
 window.ESPA.logger = logger;
 window.ESPA.tmpl = tmpl;
 
--- static_packages/utils.js
+++ static_packages/utils.js
@@ -4,3 +4,23 @@ export function getQueryString(name) {
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
+
+export function navigate(controller, viewData) {
+    ESPA.logger.log('navigate to: ' + controller);
+    ESPA.route(controller);
+    ESPA.trigger('/controllers/' + controller + '/view-data', viewData || {});        
+}
+
+export function registerRoute(controller, callback) {
+    ESPA.logger.log('registerRoute: ' + controller);
+    (function() {
+        let viewData = null;
+        ESPA.route(controller, function() {
+            viewData && callback(viewData);
+        });
+        ESPA.on('/controllers/' + controller + '/view-data', function(data) {
+            viewData = data;
+            callback(viewData);
+        });
+    })();
+}
